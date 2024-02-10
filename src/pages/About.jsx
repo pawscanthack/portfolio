b45a6b4bb8ff5654b8f3870995487fcb27f725b8
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2024-02-10 17:40:29 -0500

    Added About page placeholder content.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,43 @@
-export default function About() {
-  return <h1>About</h1>;
-}
+export const About = () => {
+  return (
+    <div className="about-page">
+      <div className="about-header">
+        <h1>Scott Davis</h1>
+        <p>Recent Cybersecurity and Network Administration Graduate</p>
+      </div>
+      <div className="summary">
+        <h2>Summary</h2>
+        <p>
+          As a recent graduate in Cybersecurity and Network Administration, I
+          bring a diverse skill set and a passion for learning to the table. I
+          excel in quickly grasping new concepts and thrive in problem-solving
+          scenarios.
+        </p>
+      </div>
+      <div className="experience">
+        <h2>Experience</h2>
+        <p>
+          - Web Development Intern at Edmonds Community College (Spring 2019)
+          <br />- Front-end lead and API development for Cybersecurity Capstone
+          Project
+        </p>
+      </div>
+      <div className="education-skills">
+        <h2>Education & Skills</h2>
+        <p>
+          - Bachelor's Degree in Cybersecurity and Systems Information from
+          Bellevue College (4.0 GPA)
+          <br />
+          - Extensive knowledge in Linux, Windows, VMWare, cloud platforms (GCP,
+          AWS, Azure, Digital Ocean), shell scripting, SQL, Python, Ansible,
+          PowerShell, PHP, JavaScript, C++
+          <br />
+          - Ability to troubleshoot TCP/IP network problems, locate
+          vulnerabilities, and use security monitoring tools
+          <br />- Proficient in project management methodologies (Scrum, Agile)
+          and communication skills
+        </p>
+      </div>
+    </div>
+  );
+};
