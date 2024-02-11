3da1b923dfb08149754894d190f68bbfa4625579
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2024-02-10 20:43:01 -0500

    Contact form working

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import Projects from "./pages/Projects";
-import { ContactMe } from "./pages/ContactMe";
+import { Contact } from "./pages/Contact";
 import "./app.css";
+
 function App() {
   return (
     <>
@@ -14,7 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/contactme" element={<ContactMe />} />
+          <Route path="/contactme" element={<Contact />} />
         </Routes>
       </div>
     </>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,63 @@
+import emailjs from "emailjs-com";
+import "./contact.css";
+
+export const Contact = () => {
+  const SERVICE_ID = "service_la59841";
+  const TEMPLATE_ID = "template_wun2qak";
+  const PUBLIC_KEY = "16vsghIyBDtJqM3_a";
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY).then(
+      (result) => {
+        alert("Message Sent Successfully");
+      },
+      (error) => {
+        console.log(error.text);
+        alert("Something went wrong!");
+      }
+    );
+    e.target.reset();
+  };
+  return (
+    <div>
+      <form class="form-container" onSubmit={handleOnSubmit}>
+        <h2>Send me a message. Let's have a chat!</h2>
+        <div class="form-element">
+          <label for="from_name">Name</label>
+          <input
+            type="text"
+            id="from_name"
+            name="from_name"
+            placeholder="Your name.."
+            required
+          />
+        </div>
+
+        <div class="form-element">
+          <label>E-mail</label>
+          <input
+            type="email"
+            id="from_email"
+            name="from_email"
+            placeholder="Your email.."
+            required
+          />
+        </div>
+
+        <div class="form-element">
+          <label for="message">Message</label>
+          <textarea
+            name="message"
+            rows="8"
+            cols="30"
+            placeholder="Your message.."
+            required
+          />
+        </div>
+        <button type="submit" className="formButton">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
