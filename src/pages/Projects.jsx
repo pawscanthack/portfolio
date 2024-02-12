bf9ba1baf8214bc650d91a3b482db5d9bfaf1541
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2024-02-11 19:19:44 -0500

    Added project links

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,17 +5,26 @@ export const Projects = () => {
   return (
     <section className="project-section">
       <div>
-        <div>
+        <div className="project-section-heading">
           <h2>Recent Projects</h2>
         </div>
         <div className="project-section-container">
           {data?.portfolio?.map((item, index) => (
             <div key={index}>
-              <div>
-                <img src={item.src} alt={item.src} />
-                <div></div>
-                <h3>{item.title}</h3>
-                <p>{item.description}</p>
+              <div className="project-content">
+                <img src={item.src} alt={item.src} className="project-image" />
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
+                <a
+                  href={item.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="project-link"
+                >
+                  Visit Project
+                </a>
               </div>
             </div>
           ))}
