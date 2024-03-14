236bbd9f6f9217181adceb0dadba0dfb1bd4e83d
red_ninja <94668102+pawscanthack@users.noreply.github.com>
2024-03-13 20:06:27 -0700

    Sd testing ga 31124 (#1)
    * Basic posthog implementation working
    
    * Page View and Contact Button tracking working

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { PostHogProvider } from "posthog-js/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PostHogProvider
+      apiKey="phc_gf3A5kJPqNTifLSxhVtBbcFGUmF9IPIbEbhsngBrl2Z"
+      options={{
+        host: "https://app.posthog.com",
+        capture_pageview: false,
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PostHogProvider>
   </React.StrictMode>
 );
