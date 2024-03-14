155ea92b34427c728ede7049d14158ae17e78825
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2024-03-13 22:33:52 -0400

    Basic posthog implementation working

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { PostHogProvider } from "posthog-js/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PostHogProvider
+      apiKey="phc_gf3A5kJPqNTifLSxhVtBbcFGUmF9IPIbEbhsngBrl2Z"
+      options={{
+        host: "https://app.posthog.com",
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PostHogProvider>
   </React.StrictMode>
 );
