76af1bcf35b0e001b75d32536e9957fc14b5e25e
red_ninja <94668102+pawscanthack@users.noreply.github.com>
2024-05-13 17:25:46 -0700

    Update Navbar.jsx
    Updated resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,6 +28,7 @@ export const Navbar = () => {
         <li>
           <Link
             to="https://acrobat.adobe.com/id/urn:aaid:sc:US:81ee833f-0756-4805-b8fb-fd4500ae7a6c"
+            to="https://acrobat.adobe.com/id/urn:aaid:sc:US:d1795db8-9dd1-4191-aef8-43a0c0b76fbb"
             target="_blank"
             rel="noreferrer"
             className="resume-link"
