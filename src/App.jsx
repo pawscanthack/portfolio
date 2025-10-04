7e09ccaf52d1ba95e69215f01ebb08f5926cf843
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2025-10-04 15:46:47 -0400

    Created new Skills page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
+import { Skills } from "./pages/Skills";
 import { Projects } from "./pages/Projects";
 import { Contact } from "./pages/Contact";
 import "./css/app.css";
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contactme" element={<Contact />} />
         </Routes>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -28,58 +28,6 @@ export const About = () => {
             enables me to provide exceptional service and help businesses thrive in a rapidly changing tech landscape.
             </p>
           </div>
-          <div className="about-content">
-            <h2>Skills</h2>
-            <ul className="skills-list-container">
-              <li>
-                Networking & Infrastructure: Proficient in TCP/IP, DHCP, DNS, VPN configuration, wireless performance optimization, and VoIP systems.
-              </li>
-              <li>
-                Troubleshooting & Operating Systems: Experienced diagnosing application, hardware, and OS issues across Windows, macOS, Linux, iOS, and Android using GUI and command-line tools.
-              </li>
-              <li>
-                Hardware Support: Skilled at repairing and provisioning client-side hardware including desktops, laptops, peripherals, mobile devices, printers, docking stations, and POS systems.
-              </li>
-              <li>
-                Directory & Identity Management: Advanced user and access administration experience in Active Directory, Entra, and Google Workspace.
-              </li>
-              <li>
-              Service Desk Platforms: Competent in Jira Service Management, AutoTask, ServiceNow, and similar ticketing systems.
-              </li>
-              <li>
-              Scripting & Automation: Proficient in Python and PowerShell for automating tasks, managing systems, and enhancing workflows.
-              </li>
-              <li>
-              Cybersecurity Fundamentals: Knowledgeable in firewalls, endpoint protection, vulnerability management, and security best practices to safeguard IT environments.
-              </li>
-            </ul>
-          </div>
-          <div className="about-content">
-            <h2>Certifications</h2>
-            <ul className="skills-list-container">
-              <li>
-              Microsoft | MD-102 Certified Endpoint Administrator	September 2025              
-              </li>
-              <li>
-              CompTia | IT Operations Specialist	July 2025
-              </li>
-              <li>
-              CompTia | Network+	July 2025
-              </li>
-              <li>
-              CompTia | A+	April 2025
-              </li>
-              <li>
-              Splunk | Certified Core User	September 2025
-              </li>
-              <li>
-              CompTia | CySA+	August 2024
-              </li>
-              <li>
-              The Python Institute | Certified Associate Programmer	May 2024              
-              </li>
-            </ul>
-          </div>
           <div className="linkedin-link-container">
             <a
               href="https://www.linkedin.com/in/scottdaviscyberpro/"
