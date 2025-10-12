75974d672624a43930d998c5c7e43faef71592ce
Gavin Hensley <gavinhensley@protonmail.com>
2025-10-12 10:38:09 -0400

    Skills section (#10)
    * Update hero image
    
    * Create container for home image and change up some colors
    
    * Adjust content and layout for home page
    
    * Add framer motion animation
    
    * Add inner shadow to image container
    
    * Add slight animation to hero section/home page
    
    * Move all sections to one page
    
    * adjust width of subheader
    
    * Adjust font sizes and spacing for resposniveness
    
    * Adjust layout of about section and image
    
    * Adjust width of text within subheader on home section
    
    * Make small adjustments to about section
    
    * Change about content to articles for semantic html
    
    * Install lucide react and adjust icons, image, and layout of about section
    
    * Fix up text size and layout a bit for about section
    
    * Adjust about containers
    
    * Adjust mobile layout of home section
    
    * Create skill card
    
    * Add skills to json file
    
    * Add some styling to the skill cards
    
    * Finish styling the skill cards and fixing the layout of the skills section
    
    * Add microsoft endpoint admin cert img
    
    * Add comptia cios cert
    
    * Add all certification images

---
+++ src/components/SkillsCard.jsx
@@ -0,0 +1,18 @@
+import "./skillsCard.css";
+
+export default function SkillsCard({ cardTitle, cardContent }) {
+
+  return (
+    <section aria-label={cardTitle} className="skills-card">
+      <h3 className="skills-card-title">{cardTitle}</h3>
+      <div className="skills-content-container">
+        {cardContent.map((item, index) => (
+          <div key={index} className="skill-item">
+            <p>{item}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+
+}
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,82 +1,29 @@
 import "../css/skills.css";
-import linkedInLogo from "../img/LI-In-Bug.png";
+import data from "../data/skills.json";
+import SkillsCard from "../components/SkillsCard";
 
 export const Skills = () => {
   return (
     <section className="skills-section">
-      <div className="skills-section-heading">
-        <h1 hidden="true">Let's get to know each other!</h1>
+      <h2>Skills</h2>
+      <div className="skills-card-container">
+        {Object.keys(data?.skills)?.map((category, index) => (
+          <SkillsCard
+            key={index}
+            cardTitle={category}
+            cardContent={data.skills[category]}
+          />
+        ))}
       </div>
-      <div className="skills-section-container">
-        <div className="skills-content-container">
-          <div className="skills-content">
-            <h2>Skills</h2>
-            <ul className="skills-list-container">
-              <li>
-                Networking & Infrastructure: Proficient in TCP/IP, DHCP, DNS, VPN configuration, wireless performance optimization, and VoIP systems.
-              </li>
-              <li>
-                Troubleshooting & Operating Systems: Experienced diagnosing application, hardware, and OS issues across Windows, macOS, Linux, iOS, and Android using GUI and command-line tools.
-              </li>
-              <li>
-                Hardware Support: Skilled at repairing and provisioning client-side hardware including desktops, laptops, peripherals, mobile devices, printers, docking stations, and POS systems.
-              </li>
-              <li>
-                Directory & Identity Management: Advanced user and access administration experience in Active Directory, Entra, and Google Workspace.
-              </li>
-              <li>
-              Service Desk Platforms: Competent in Jira Service Management, AutoTask, ServiceNow, and similar ticketing systems.
-              </li>
-              <li>
-              Scripting & Automation: Proficient in Python and PowerShell for automating tasks, managing systems, and enhancing workflows.
-              </li>
-              <li>
-              Cybersecurity Fundamentals: Knowledgeable in firewalls, endpoint protection, vulnerability management, and security best practices to safeguard IT environments.
-              </li>
-            </ul>
-          </div>
-          <div className="skills-content">
-            <h2>Certifications</h2>
-            <ul className="skills-list-container">
-              <li>
-              Microsoft | MD-102 Certified Endpoint Administrator	September 2025              
-              </li>
-              <li>
-              CompTia | IT Operations Specialist	July 2025
-              </li>
-              <li>
-              CompTia | Network+	July 2025
-              </li>
-              <li>
-              CompTia | A+	April 2025
-              </li>
-              <li>
-              Splunk | Certified Core User	September 2025
-              </li>
-              <li>
-              CompTia | CySA+	August 2024
-              </li>
-              <li>
-              The Python Institute | Certified Associate Programmer	May 2024              
-              </li>
-            </ul>
-          </div>
-          <div className="linkedin-link-container">
-            <a
-              href="https://www.linkedin.com/in/scottdaviscyberpro/"
-              target="_blank"
-              rel="noreferrer"
-              className="linkedin-link"
-            >
-              <img
-                src={linkedInLogo}
-                alt="LinkedIn Logo"
-                className="linkedin-logo"
-              />
-              <p>Visit my LinkedIn page</p>
-            </a>
-          </div>
-        </div>
+      <h2>Certifications</h2>
+      <div className="cert-list">
+        <img src="/images/endpoint-administrator.webp" alt="Microsoft Endpoint Administrator" />
+        <img src="/images/comptia-cios.webp" alt="CompTia IT Operations Specialist" />
+        <img src="/images/network+.webp" alt="CompTia Network+" />
+        <img src="/images/a+.webp" alt="CompTia A+" />
+        <img src="/images/cysa+.webp" alt="CompTia CySA+" />
+        <img src="/images/splunk-core-user.webp" alt="Splunk Certified Core User" />
+        <img src="/images/pcap.webp" alt="Python Institute Certified Associate Programmer" />
       </div>
     </section>
   );
